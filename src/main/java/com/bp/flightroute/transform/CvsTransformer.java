99b2215101aa65dc6b3227dc4ae2bc0278c4c5e4
Brendan Pierce <qaspok@gmail.com>
2016-08-16 22:52:20 -0400

    quick and dirty processing of csv file

--- src/main/java/com/bp/flightroute/transform/CvsTransformer.java
+++ src/main/java/com/bp/flightroute/transform/CvsTransformer.java
@@ -4,39 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.bp.flightroute.model.Flight;
 import com.bp.flightroute.model.Flights;
 
 public class CvsTransformer {
 
 	public static Flights process(InputStream cvs) {
-		
-		System.out.println(getStringFromInputStream(cvs));
-		return null;
+
+		Flights flights = new Flights();
+		List<String> rows = getLinesFromInputStream(cvs);
+		if (rows != null && rows.size() > 1) {
+			for (int r = 1; r < rows.size(); r++) {
+				String[] column = rows.get(r).split(",");
+				Flight flight = new Flight();
+				flight.setStartname(column[0]);
+				flight.setStartcode(column[1]);
+				flight.setEndname(column[2]);
+				flight.setEndcode(column[3]);
+				flight.setCost(Double.parseDouble(column[4]));
+				flights.getFlights().add(flight);
+			}
+		}
+		System.out.println(flights);
+		return flights;
 	}
 
-	private static String getStringFromInputStream(InputStream is) {
+	private static List<String> getLinesFromInputStream(InputStream is) {
 
-		BufferedReader br = null;
-		StringBuilder sb = new StringBuilder();
-		String line;
-		try {
-			br = new BufferedReader(new InputStreamReader(is));
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
+		List<String> rows = new ArrayList<String>();
+		String row = null;
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+			while ((row = br.readLine()) != null) {
+				rows.add(row);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
-		
-		return sb.toString();
+		return rows;
 	}
 }
